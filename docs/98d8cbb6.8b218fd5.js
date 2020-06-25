5d96b86a2667ea8ac6f44ae56c79b94206972e02
Matt Rapczynski <matt@cloudmation.io>
2020-06-24 18:31:57 -0700

    Removed local mode for automated releases

--- docs/98d8cbb6.8b218fd5.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{106:function(o){o.exports=JSON.parse('{"docsSidebars":{"mainSidebar":[{"type":"link","label":"About","href":"/cvc-oei-sis-tools/docs/about"}]},"permalinkToSidebar":{"/cvc-oei-sis-tools/docs/about":"mainSidebar"}}')}}]);
---
+++ docs/c4f5d8e4.c5916fe7.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{108:function(n,t,e){"use strict";e.r(t);var a=e(0),o=e.n(a),c=e(118),u=e(113);t.default=function(){return o.a.createElement(c.a,{to:Object(u.a)("/docs/about")})}}}]);
